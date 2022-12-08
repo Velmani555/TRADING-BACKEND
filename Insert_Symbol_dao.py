f872273cb134ad554a010f213ea3e2cf78173c42
velmani555 <vmani3479@gmail.com>
2022-12-08 18:47:02 +0530

    CRUD

--- Insert_Symbol_dao.py
+++ Insert_Symbol_dao.py
@@ -2,7 +2,7 @@ import datetime
 from dbconnection import DBConnection
 
 
-class SymbolDAO:
+class SymbolInsertDAO:
     def insert_symbolDAO(self, symbol_id, symbol_code, symbol_name, sector_name, sector_code, created_date, updated_date, created_by, updated_by, symbol_status):
         dbconnection = DBConnection()
         connection = dbconnection.getconnection()
@@ -13,7 +13,7 @@ class SymbolDAO:
         connection.close()
 
 
-symbol_id = 4
+symbol_id = 111
 symbol_name = "SBINN"
 symbol_code = "SBBBB"
 sector_name = "bank"
@@ -24,6 +24,6 @@ created_by = 101
 updated_by = 101
 symbol_status = 1
 
-symbolDAO = SymbolDAO()
-symbolDAO.insert_symbolDAO(symbol_id, symbol_name, symbol_code, sector_name,
-                           sector_code, created_date, updated_date, created_by, updated_by, symbol_status)
+symbolinsertDAO = SymbolInsertDAO()
+symbolinsertDAO.insert_symbolDAO(symbol_id, symbol_name, symbol_code, sector_name,
+                                 sector_code, created_date, updated_date, created_by, updated_by, symbol_status)
--- update_symbol_dao.py
+++ update_symbol_dao.py
@@ -2,28 +2,16 @@ import datetime
 from dbconnection import DBConnection
 
 
-class SymbolDAO:
-    def insert_symbolDAO(self, symbol_id, symbol_code, symbol_name, sector_name, sector_code, created_date, updated_date, created_by, updated_by, symbol_status):
+class SymbolUpdateDAO:
+    def update_symbolDAO(self):
         dbconnection = DBConnection()
         connection = dbconnection.getconnection()
         cursor = connection.cursor()
         cursor.execute(
-            '''insert into symbol_list (symbol_id, symbol_code, symbol_name, sector_name,sector_code,created_date,updated_date,created_by,updated_by,symbol_status) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);''', (symbol_id, symbol_code, symbol_name, sector_name, sector_code, created_date, updated_date, created_by, updated_by, symbol_status))
+            '''update symbol_list set symbol_id=5 where symbol_id=8;''')
         connection.commit()
         connection.close()
 
 
-symbol_id = 4
-symbol_name = "SBINN"
-symbol_code = "SBBBB"
-sector_name = "bank"
-sector_code = 10
-created_date = "2021-06-12"
-updated_date = "2022-07-12"
-created_by = 101
-updated_by = 101
-symbol_status = 1
-
-symbolDAO = SymbolDAO()
-symbolDAO.insert_symbolDAO(symbol_id, symbol_name, symbol_code, sector_name,
-                           sector_code, created_date, updated_date, created_by, updated_by, symbol_status)
+symbolDAO = SymbolUpdateDAO()
+symbolDAO.update_symbolDAO()
